b627794ef57e464ae90e4f2c0dcb666f054ddd3d
shashikant139 <mailshashi534@gmail.com>
2024-06-01 18:39:28 +0530

    project2

--- src/App.js
+++ src/App.js
@@ -2,23 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let str = "Hello"
-  let str2 = "World"
+//   let str = "Hello"
+//   let str2 = "World"
 
-  if(str === "Hello" && str2 === "World")
-    {
-      console.log("Hello World")
-    }
-const collage_address = {
-  collage_name: "rvscet",
-  collage_address:"jamshedpur",
-}
+//   if(str === "Hello" && str2 === "World")
+//     {
+//       console.log("Hello World")
+//     }
+// const collage1 = {
+//   name: "rvscet",
+//   address:"jamshedpur",
+// };
+// const college2 = {
+//   name: "rvscet",
+//   adress:ranchi,
+// };
 
-if(collage_address === "rvscet")
-    {
+// const fun = () =>{
+//   console.log(5+5)
+// }
+// if(collage1,name1 === college2.name2)
+//     {
+//       fun()
+// console.log("hello world")
+//     }else{
 
-    }
-
+//     }console.log("unknown")
+  const arr1= [1,2,3,4]
+  for (let item of arr1){
+    console.log(item)
+  }
   return (
     <div className="App">
       <header className="App-header">
