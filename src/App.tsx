a2649b67ecf96337c01fb1dcbb0b94085ca05097
shashikant139 <mailshashi534@gmail.com>
2024-06-11 17:34:07 +0530

    use of increment decrement

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,39 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [data, setData] = useState<string>();
+  const [data, setData] = useState<number>(0);
+  const [numberData, setNumberData] = useState<number>(0);
 
   useEffect(() => {
-    setData("world");
-  }, []);
+    addData();
+  }, [numberData]);
+
+  const addData = () => {
+    const oldData = data;
+    const newData = oldData + 1;
+    setData(newData);
+    console.log(newData);
+  };
+  
+  const decrementData =() => {
+    setNumberData(numberData - 1);
+  };
 
   return (
     <>
-    <div>{data}</div>
-    </>
-  )
+    <button title="title" onClick={addData} className={`number ${data}`}>
+      addData
+    </button>
+
+    <div> Incremented Data: {data}</div>
+    <br />
+
+    <button onClick={decrementData} className={`decrement ${numberData}`}>
+      decrement
+      </button>
+    <div>deceremented Data: {numberData}</div>
+</>
+  );
 }
 
 export default App
